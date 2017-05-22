47b0732d1dcc66bb075c26463a55855d039bb1f9
kharland <kendaljharland@gmail.com>
2017-05-22 09:09:41 -0700

    Add convenience methods for variable bits and clean up flattened patterns

--- lib/bit_pattern.dart
+++ lib/bit_pattern.dart
@@ -3,12 +3,22 @@ library bit_pattern;
 import 'package:binary/binary.dart';
 import 'package:collection/collection.dart';
 import 'package:meta/meta.dart';
+import 'package:quiver/strings.dart';
+
+/// Returns a variable bit, optionally named [name].
+_Variable bit([String name = '']) => bits(1, name);
+
+/// Returns a variable 4-bit chunk, optionally named [name].
+_Variable nibble([String name = '']) => bits(4, name);
+
+/// Returns a variable 8-bit chunk, optionally named [name].
+_Variable byte([String name = '']) => bits(8, name);
 
 /// Returns a variable chunk of a [BitPattern] with [length] and [name].
 ///
 /// Both [name] and [length] are used when converting a [BitPattern] to a
 /// string.
-_Variable v(int length, String name) => new _Variable(length, name);
+_Variable bits(int length, [String name = '']) => new _Variable(length, name);
 
 /// Allows matching integer values against a collection of [BitPattern].
 class BitPatternGroup {
@@ -68,26 +78,17 @@ class BitPatternGroup {
 /// The sequence may contain only 0's and 1's and variable length chunks.  The
 /// pattern's indices can be queried using [is0], [is1] and [isVar], and the
 /// pattern can be matched against integer values.  Variable sections in a
-/// sequence can be represented using [v].
+/// sequence can be represented using [bits].
 class BitPattern {
-  static const _F0 = '0';
-  static const _F1 = '1';
-  static const _FVAR = 'v';
-
-  final List _parts;
+  final _parts = <_PatternPart>[];
   final _flattened = <String>[];
   String _asString;
 
-  BitPattern(this._parts) {
-    for (var part in _parts) {
-      if (part is int) {
-        assert(part == 0 || part == 1);
-        _flattened.add(part == 1 ? _F1 : _F0);
-      } else {
-        assert(part is _Variable);
-        _flattened.addAll(new List.generate(part.length, (_) => _FVAR));
-      }
-    }
+  BitPattern(List<_PatternPart> parts) {
+    _parts.addAll(parts.map((p) => new _PatternPart.from(p)));
+    _parts.forEach((part) {
+      _flattened.addAll(part.flattened);
+    });
   }
 
   @override
@@ -100,13 +101,13 @@ class BitPattern {
   int get length => _flattened.length;
 
   /// Returns true iff bit [n] is 0.
-  bool is0(int n) => n < length && _flattened[length - n - 1] == _F0;
+  bool is0(int n) => n < length && _flattened[length - n - 1] == _FLAT0;
 
   /// Returns true iff bit [n] is 1.
-  bool is1(int n) => n < length && _flattened[length - n - 1] == _F1;
+  bool is1(int n) => n < length && _flattened[length - n - 1] == _FLAT1;
 
   /// Returns true iff bit [n] is variable.
-  bool isVar(int n) => n < length && _flattened[length - n - 1] == _FVAR;
+  bool isVar(int n) => n < length && _flattened[length - n - 1] == _FLATV;
 
   /// Returns true iff the integer [bits] matches this pattern.
   ///
@@ -143,21 +144,9 @@ class BitPattern {
 
   String _computeAsString() {
     final buffer = new StringBuffer();
-
     _parts.forEach((part) {
-      if (part == 0 || part == 1) {
-        buffer.write('$part');
-      } else {
-        assert(part is _Variable);
-        if (part.length > 1) {
-          buffer.write('${part.name}{${part.length}}');
-        } else {
-          buffer.write('${part.name}');
-        }
-      }
-      buffer.write(' ');
+      buffer.write('$part ');
     });
-
     return buffer.toString().trimRight();
   }
 }
@@ -167,16 +156,69 @@ class BitPatternException implements Exception {
   const BitPatternException();
 }
 
+/// Part of a [BitPattern].  Either a 1, 0 or variable length sequence.
+abstract class _PatternPart {
+  factory _PatternPart.from(part) {
+    if (part is int) {
+      assert(part == 0 || part == 1);
+      return new _Bit(part);
+    } else {
+      assert(part is _Variable);
+      return part;
+    }
+  }
+
+  /// The flattened representation of this part.
+  List<String> get flattened;
+}
+
+class _Bit implements _PatternPart {
+  final int bit;
+
+  /// A single-item list containing [bit] as a string.
+  @override
+  final List<String> flattened;
+
+  _Bit(this.bit) : flattened = <String>['$bit'];
+
+  @override
+  String toString() => '$bit';
+}
+
 /// Represents a variable chunk of bits in a [BitPattern].
-class _Variable {
+class _Variable implements _PatternPart {
   /// The number of bits in this chunk.
   final int length;
 
   /// The name of this chunk.
   final String name;
 
-  _Variable(this.length, this.name);
+  /// A single-item list containing [bit] as a string.
+  @override
+  final List<String> flattened;
+
+  _Variable(this.length, this.name)
+      : flattened = new List.generate(length, (_) => _FLATV);
+
+  @override
+  String toString() {
+    var nameStr = isEmpty(name) ? '?' : name;
+    if (length > 1) {
+      return '$nameStr{$length}';
+    } else {
+      return '$nameStr';
+    }
+  }
 }
+
+/// Flattened representation of a 1 bit
+const _FLAT1 = '1';
+
+/// Flattened representation of a 0 bit
+const _FLAT0 = '0';
+
+/// Flattened representation of a variable bit
+const _FLATV = 'x';
 
 /// A measure of how specific a [BitPattern] is relative another [BitPattern].
 enum _Specificity { greater, lesser, equal }
